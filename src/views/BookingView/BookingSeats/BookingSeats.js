98cb09c674c8c3e1f07f56d960dca58c93d718a8
DuongYi <duonguet1903@gmail.com>
2021-04-19 17:46:08 +0700

    add SeatBooking

--- src/views/BookingView/BookingSeats/BookingSeats.js
+++ src/views/BookingView/BookingSeats/BookingSeats.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {
-  Box, Grid, Typography, makeStyles
+  Box, Grid, makeStyles
 } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
@@ -38,11 +38,67 @@ const useStyles = makeStyles((theme) => ({
     width: 10,
     height: 10
   },
-
+  container: {
+    marginTop: '12vh',
+    marginBottom: '12vh',
+    perspective: '1000px',
+    height: '100%'
+  },
+  Seat: {
+    cursor: 'pointer',
+    backgroundColor: 'rgb(212 204 205)',
+    height: 20,
+    width: 25,
+    margin: 6,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10
+  },
+  seatOccupied: {
+    cursor: 'pointer',
+    backgroundColor: 'rgb(65, 66, 70)',
+    height: 20,
+    width: 25,
+    margin: 6,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10
+  },
+  row1: {
+    display: 'flex'
+  },
+  screen: {
+    backgroundColor: '#fff',
+    height: 70,
+    width: '100%',
+    margin: '15px 0',
+    transform: 'rotateX(-45deg)',
+    boxShadow: '0 3px 10px rgba(255, 255, 255, 0.7)'
+  },
   [theme.breakpoints.down('sm')]: {
     seat: { padding: theme.spacing(1.2), margin: theme.spacing(0.5) },
     seatInfoContainer: { width: '100%', display: 'block' },
-    seatInfo: { marginTop: theme.spacing(2) }
+    seatInfo: { marginTop: theme.spacing(2) },
+    container: {
+      paddingLeft: theme.spacing(15),
+      paddingRight: theme.spacing(3),
+      marginTop: '3vh',
+      marginBottom: '0vh',
+    }
+  },
+  [theme.breakpoints.down('md')]: {
+    Seat: {
+      height: 12,
+      width: 15,
+      margin: 4,
+      borderTopLeftRadius: 8,
+      borderTopRightRadius: 8
+    },
+    seatOccupied: {
+      height: 12,
+      width: 15,
+      margin: 4,
+      borderTopLeftRadius: 8,
+      borderTopRightRadius: 8
+    }
   }
 }));
 
@@ -51,11 +107,166 @@ export default function BookingSeats(props) {
 
   return (
     <>
-      <Box height="58vh">
+      <Box>
         <Grid container alignItems="center" justify="center">
-          <Typography variant="h2" style={{ marginTop: '27vh' }}>
-            I do not know how to do it :)
-          </Typography>
+          <div className={classes.container}>
+            <div className={classes.screen} />
+
+            <div className={classes.row1}>
+
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} style={{ marginRight: 20 }} />
+
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+
+              <div className={classes.Seat} style={{ marginLeft: 20 }} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+            </div>
+
+            <div className={classes.row1}>
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} style={{ marginRight: 20 }} />
+
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+
+              <div className={classes.Seat} style={{ marginLeft: 20 }} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+            </div>
+
+            <div className={classes.row1}>
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} style={{ marginRight: 20 }} />
+
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+
+              <div className={classes.seatOccupied} style={{ marginLeft: 20 }} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+            </div>
+
+            <div className={classes.row1}>
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} style={{ marginRight: 20 }} />
+
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+
+              <div className={classes.seatOccupied} style={{ marginLeft: 20 }} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+            </div>
+
+            <div className={classes.row1}>
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} style={{ marginRight: 20 }} />
+
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+
+              <div className={classes.Seat} style={{ marginLeft: 20 }} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+            </div>
+
+            <div className={classes.row1}>
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} style={{ marginRight: 20 }} />
+
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+
+              <div className={classes.Seat} style={{ marginLeft: 20 }} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+            </div>
+
+            <div className={classes.row1}>
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} style={{ marginRight: 20 }} />
+
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+              <div className={classes.Seat} />
+
+              <div className={classes.seatOccupied} style={{ marginLeft: 20 }} />
+              <div className={classes.seatOccupied} />
+              <div className={classes.Seat} />
+            </div>
+          </div>
         </Grid>
       </Box>
       <Box width={1} mt={10}>
@@ -63,7 +274,7 @@ export default function BookingSeats(props) {
           <div className={classes.seatInfo}>
             <div
               className={classes.seatInfoLabel}
-              style={{ background: 'rgb(96, 93, 169)' }}
+              style={{ background: 'rgb(212 204 205)' }}
             />
             Seat Available
           </div>
